cd09e11a21d5b7e9da2181923b62c44d38990cfd
IhorHolinko <mariupolprosecutor@gmail.com>
2023-07-19 13:30:30 +0200

    Portfolio2023HtmlCss

---
+++ js/main.js
@@ -0,0 +1,50 @@
+const btnDarkMode = document.querySelector(".dark-mode-btn");
+
+
+
+
+
+// 1. Check dark mode on system level
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark");
+}
+
+// 2. Check dark mode in localStorage
+if (localStorage.getItem("darkMode") === "dark") {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark");
+} else if (localStorage.getItem("darkMode") === "light") {
+    btnDarkMode.classList.remove("dark-mode-btn--active");
+    document.body.classList.remove("dark");
+}
+
+// 3. If system changed - change thema
+window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", (event) => {
+        const newColorScheme = event.matches ? "dark" : "light";
+
+        if (newColorScheme === "dark") {
+            btnDarkMode.classList.add("dark-mode-btn--active");
+            document.body.classList.add("dark");
+            localStorage.setItem("darkMode", "dark");
+        } else {
+            btnDarkMode.classList.remove("dark-mode-btn--active");
+            document.body.classList.remove("dark");
+        }
+});
+
+// Turn on-off Night mode
+btnDarkMode.onclick = function () {
+    btnDarkMode.classList.toggle("dark-mode-btn--active");
+    const isDark = document.body.classList.toggle("dark");
+
+    if (isDark) {
+        localStorage.setItem("darkMode", "dark");
+    } else {
+        localStorage.setItem("darkMode", "light");
+    }
+
+}
+
